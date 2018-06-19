c664ddf0891b00a2023974a614feebc68398680b
Justin Wells <justinwells@justins-imac.local>
2018-06-19 10:48:33 -0400

    Initial Commit

---
+++ Oculus/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Oculus
+//
+//  Created by Justin Wells on 6/19/18.
+//  Copyright © 2018 SynergyLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Oculus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Oculus
+//
+//  Created by Justin Wells on 6/19/18.
+//  Copyright © 2018 SynergyLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
